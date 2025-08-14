8c1c84cfb52bfa8cc36beb0d0d973108ada52464
D.Lakshmi Narasimha <narasimha34327@gmail.com>
2025-08-14 11:22:59 +0530

    resolved config errors

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -1,8 +1,12 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
-
+//.env file is loaded from root folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '../../.env') });
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
